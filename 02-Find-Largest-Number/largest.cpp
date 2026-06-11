4745e6cda41662c220c60fe6a3bc46c967fcf765
JRK <reemuskumar@gmail.com>
2026-06-11 17:00:45 +0530

    Optimize loop to find largest number using ranges
    Use ranges to skip the first element in the loop.

--- 02-Find-Largest-Number/largest.cpp
+++ 02-Find-Largest-Number/largest.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <format>
+#include <ranges>
 
 int main() {
     using namespace std;
@@ -17,10 +18,11 @@ int main() {
     }
 
     int largest_num = num_list[0];
-    for (const int& num : num_list) {
+    for (const int& num : num_list | std::views::drop(1)) {
         if (num > largest_num) {
             largest_num = num;
         }
+        cout << num << endl;
     }
     cout << format("Largest Number is {}", largest_num) << endl;
     return 0;

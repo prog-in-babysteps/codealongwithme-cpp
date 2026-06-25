34ffe1f81f805df63b921578cde0626d8af73df9
JRK <reemuskumar@gmail.com>
2026-06-25 16:41:16 +0530

    Implement program to find second largest number

---
+++ 03-Second-Largest/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <format>
+
+int main() {
+    using namespace std;
+
+    int num_input;
+    cout << "No. of Inputs: ";
+    cin >> num_input;
+
+    // Create a vector with the size of N element
+    vector<int> num_list(num_input);
+
+    // Read the input numbers and append it to the vector
+    for (int i = 0; i < num_input; ++i) {
+        cin >> num_list[i];
+    }
+
+    // Compare first 2 numbers to get largest and second largest
+    int largest_num, second_largest_num;
+    if (num_list[0] > num_list[1]) {
+        largest_num = num_list[0];
+        second_largest_num = num_list[1];
+    } else {
+        largest_num = num_list[1];
+        second_largest_num = num_list[0];
+    }
+    for (int i = 2; i < num_list.size(); i++) {
+        if (num_list[i] > largest_num) {
+            second_largest_num = largest_num;
+            largest_num = num_list[i];
+        } else if (num_list[i] > second_largest_num) {
+            second_largest_num = num_list[i];
+        }
+    }
+    cout << format("Largest Number is {}", largest_num) << endl;
+    cout << format("Second Largest Number is {}", second_largest_num) << endl;
+}
